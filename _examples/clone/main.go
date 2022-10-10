19edc0419c5e60bda7f504d83d4a739ce6d481a3
Ramiro Castillo <43893061+ramiroJCB@users.noreply.github.com>
2022-10-10 09:53:24 -0500

    chore(clone): Leaving the config example

--- _examples/clone/main.go
+++ _examples/clone/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	//configTest := map[string]string{"core.eol": "true"}
-	err := clone.Exec(context.Background(), args[0], args[1], clone.WithBare(true))
+	configTest := map[string]string{"core.eol": "true"}
+	err := clone.Exec(context.Background(), args[0], args[1], clone.WithConfig(configTest))
 	if err != nil {
 		log.Fatal(err)
 	}

ecf0f1bddd30408fe9fa7ca11d982bf93aa8025c
Ramiro Castillo <43893061+ramiroJCB@users.noreply.github.com>
2022-10-10 11:56:37 -0500

    chore(clone): separate  cloning tests

--- clone/clone_test.go
+++ clone/clone_test.go
@@ -9,55 +9,51 @@ import (
 
 func TestSingleArgsOK(t *testing.T) {
 	type test struct {
-		options      []Option
-		flags        []string
-		path         string
-		spectedError bool
+		options []Option
+		flags   []string
 	}
 
 	tests := []test{
-		{options: []Option{WithRejectShallow(true)}, flags: []string{"--reject-shallow"}, spectedError: false},
-		{options: []Option{WithNoRejectShallow(true)}, flags: []string{"--no-reject-shallow"}, spectedError: false},
-		{options: []Option{WithNoCheckout(true)}, flags: []string{"--no-checkout"}, spectedError: false},
-		{options: []Option{WithBare(true)}, flags: []string{"--bare"}, spectedError: false},
-		{options: []Option{WithMirror(true)}, flags: []string{"--mirror"}, spectedError: false},
-		{options: []Option{WithLocal(true)}, flags: []string{"--local"}, spectedError: false},
-		{options: []Option{WithNoHardLinks(true)}, flags: []string{"--no-hardlinks"}, spectedError: false},
-		{options: []Option{WithShared(true)}, flags: []string{"--shared"}, spectedError: false},
-		{options: []Option{WithDissociate(true)}, flags: []string{"--dissociate"}, spectedError: false},
-		{options: []Option{WithQuiet(true)}, flags: []string{"--quiet"}, spectedError: false},
-		{options: []Option{WithVerbose(true)}, flags: []string{"--verbose"}, spectedError: false},
-		{options: []Option{WithProgress(true)}, flags: []string{"--progress"}, spectedError: false},
-		{options: []Option{WithSparse(true)}, flags: []string{"--sparse"}, spectedError: false},
-		{options: []Option{WithSingleBranch(true)}, flags: []string{"--single-branch"}, spectedError: false},
-		{options: []Option{WithNoSingleBranch(true)}, flags: []string{"--no-single-branch"}, spectedError: false},
-		{options: []Option{WithNoTags(true)}, flags: []string{"--no-tags"}, spectedError: false},
-		{options: []Option{WithNoShallowSubmodules(true)}, flags: []string{"--no-shallow-submodules"}, spectedError: false},
-		{options: []Option{WithRemoteSubmodules(true)}, flags: []string{"--remote-submodules"}, spectedError: false},
-		{options: []Option{WithNoRemoteSubmodules(true)}, flags: []string{"--no-remote-submodules"}, spectedError: false},
-		{options: []Option{WithFilter("some-filter")}, flags: []string{"--filter=some-filter"}, spectedError: false},
-		{options: []Option{WithRecurseSubmodules("some-string")}, flags: []string{"--recurse-submodules=some-string"}, spectedError: false},
-		{options: []Option{WithAlsoFilterSubmodules(true)}, flags: []string{"--also-filter-submodules"}, spectedError: false},
-		{options: []Option{WithServerOptions([]string{"a", "b"})}, flags: []string{"--server-option", "a", "--server-option", "b"}, spectedError: false},
-		{options: []Option{WithShallowExclude([]string{"a", "b"})}, flags: []string{"--shallow-exclude=a", "--shallow-exclude=b"}, spectedError: false},
-		{options: []Option{WithSeparateGitDir("some-string")}, flags: []string{"--separate-git-dir=some-string"}, spectedError: false},
-		{options: []Option{WithShallowSince("some-string")}, flags: []string{"--shallow-since", "some-string"}, spectedError: false},
-		{options: []Option{WithTemplate("some-string")}, flags: []string{"--template", "some-string"}, spectedError: false},
-		{options: []Option{WithUploadPack("some-string")}, flags: []string{"--upload-pack", "some-string"}, spectedError: false},
-		{options: []Option{WithOrigin("some-string")}, flags: []string{"--origin", "some-string"}, spectedError: false},
-		{options: []Option{WithReference("some-string")}, flags: []string{"--reference", "some-string"}, spectedError: false},
-		{options: []Option{WithReferenceIfAble("some-string")}, flags: []string{"--reference-if-able", "some-string"}, spectedError: false},
-		{options: []Option{WithBranch("some-string")}, flags: []string{"--branch", "some-string"}, spectedError: false},
-		{options: []Option{WithDepth(5)}, flags: []string{"--depth=5"}, spectedError: false},
-		{options: []Option{WithJobs(5)}, flags: []string{"--jobs=5"}, spectedError: false},
-		{options: []Option{WithConfig([]ConfigKV{{Key: "a", Value: "b"}, {Key: "c", Value: "d"}})}, flags: []string{"--config=a=b", "--config=c=d"}, spectedError: false},
-		{options: []Option{WithBare(true)}, flags: []string{"--bare"}, path: "", spectedError: true},
+		{options: []Option{WithRejectShallow(true)}, flags: []string{"--reject-shallow"}},
+		{options: []Option{WithNoRejectShallow(true)}, flags: []string{"--no-reject-shallow"}},
+		{options: []Option{WithNoCheckout(true)}, flags: []string{"--no-checkout"}},
+		{options: []Option{WithBare(true)}, flags: []string{"--bare"}},
+		{options: []Option{WithMirror(true)}, flags: []string{"--mirror"}},
+		{options: []Option{WithLocal(true)}, flags: []string{"--local"}},
+		{options: []Option{WithNoHardLinks(true)}, flags: []string{"--no-hardlinks"}},
+		{options: []Option{WithShared(true)}, flags: []string{"--shared"}},
+		{options: []Option{WithDissociate(true)}, flags: []string{"--dissociate"}},
+		{options: []Option{WithQuiet(true)}, flags: []string{"--quiet"}},
+		{options: []Option{WithVerbose(true)}, flags: []string{"--verbose"}},
+		{options: []Option{WithProgress(true)}, flags: []string{"--progress"}},
+		{options: []Option{WithSparse(true)}, flags: []string{"--sparse"}},
+		{options: []Option{WithSingleBranch(true)}, flags: []string{"--single-branch"}},
+		{options: []Option{WithNoSingleBranch(true)}, flags: []string{"--no-single-branch"}},
+		{options: []Option{WithNoTags(true)}, flags: []string{"--no-tags"}},
+		{options: []Option{WithNoShallowSubmodules(true)}, flags: []string{"--no-shallow-submodules"}},
+		{options: []Option{WithRemoteSubmodules(true)}, flags: []string{"--remote-submodules"}},
+		{options: []Option{WithNoRemoteSubmodules(true)}, flags: []string{"--no-remote-submodules"}},
+		{options: []Option{WithFilter("some-filter")}, flags: []string{"--filter=some-filter"}},
+		{options: []Option{WithRecurseSubmodules("some-string")}, flags: []string{"--recurse-submodules=some-string"}},
+		{options: []Option{WithAlsoFilterSubmodules(true)}, flags: []string{"--also-filter-submodules"}},
+		{options: []Option{WithServerOptions([]string{"a", "b"})}, flags: []string{"--server-option", "a", "--server-option", "b"}},
+		{options: []Option{WithShallowExclude([]string{"a", "b"})}, flags: []string{"--shallow-exclude=a", "--shallow-exclude=b"}},
+		{options: []Option{WithSeparateGitDir("some-string")}, flags: []string{"--separate-git-dir=some-string"}},
+		{options: []Option{WithShallowSince("some-string")}, flags: []string{"--shallow-since", "some-string"}},
+		{options: []Option{WithTemplate("some-string")}, flags: []string{"--template", "some-string"}},
+		{options: []Option{WithUploadPack("some-string")}, flags: []string{"--upload-pack", "some-string"}},
+		{options: []Option{WithOrigin("some-string")}, flags: []string{"--origin", "some-string"}},
+		{options: []Option{WithReference("some-string")}, flags: []string{"--reference", "some-string"}},
+		{options: []Option{WithReferenceIfAble("some-string")}, flags: []string{"--reference-if-able", "some-string"}},
+		{options: []Option{WithBranch("some-string")}, flags: []string{"--branch", "some-string"}},
+		{options: []Option{WithDepth(5)}, flags: []string{"--depth=5"}},
+		{options: []Option{WithJobs(5)}, flags: []string{"--jobs=5"}},
+		{options: []Option{WithConfig([]ConfigKV{{Key: "a", Value: "b"}, {Key: "c", Value: "d"}})}, flags: []string{"--config=a=b", "--config=c=d"}},
 	}
 
 	for _, tc := range tests {
 		t.Run(strings.Join(tc.flags, ","), func(t *testing.T) {
 
-			ctx := context.Background()
 			o := &execOptions{}
 
 			// apply the options
@@ -71,14 +67,33 @@ func TestSingleArgsOK(t *testing.T) {
 				t.Errorf("got %v, want %v", got, tc.flags)
 			}
 
-			if tc.spectedError {
+		})
+	}
+}
 
-				err := Exec(ctx, "https://github.com/mergestat/gitutils", tc.path, tc.options[0])
+func TestInvalidPath(t *testing.T) {
+	type test struct {
+		options      []Option
+		flags        []string
+		path         string
+		spectedError bool
+	}
 
-				if err == nil {
-					t.Fatal(err)
-				}
+	tests := []test{
+		{options: []Option{WithBare(true)}, flags: []string{"--bare"}, path: "", spectedError: true},
+	}
+
+	for _, tc := range tests {
+		t.Run(strings.Join(tc.flags, ","), func(t *testing.T) {
+
+			ctx := context.Background()
+
+			err := Exec(ctx, "https://github.com/mergestat/gitutils", tc.path, tc.options[0])
+
+			if err == nil {
+				t.Fatal(err)
 			}
+
 		})
 	}
 }

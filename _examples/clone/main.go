a747400f12ff452cd05b8562d1d43b20960a297f
Patrick DeVivo <patrick.devivo@gmail.com>
2022-10-10 19:34:15 -0400

    fix: handle stderr error output in `git clone`

--- _examples/clone/main.go
+++ _examples/clone/main.go
@@ -4,15 +4,18 @@ import (
 	"context"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/mergestat/gitutils/clone"
 )
 
 func main() {
 	args := os.Args[1:]
-	configTest := map[string]string{"core.eol": "true"}
-	err := clone.Exec(context.Background(), args[0], args[1], clone.WithConfig(configTest))
+	err := clone.Exec(context.Background(), args[0], args[1])
 	if err != nil {
+		if err, ok := err.(*exec.ExitError); ok {
+			log.Fatal(string(err.Stderr)+"\n", err)
+		}
 		log.Fatal(err)
 	}
 }
